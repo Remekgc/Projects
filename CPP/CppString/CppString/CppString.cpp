207a26aa9b48c025cc1b316630d5cb2f9419a7d6
Remekgc <RemigiuszGA335@nemi.tskoli.is>
2018-12-09 21:57:39 +0100

    Add files via upload

---
+++ CPP/CppString/CppString/CppString.cpp
@@ -0,0 +1,44 @@
+#include "pch.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string s1;
+	string s2{ "Frank" };
+	string s3{ s2 };
+	string s4{ "Frank", 3 }; // Fra
+	string s5{ s3, 0, 2 }; // Fr
+	string s6(3, 'X'); // XXX
+	string s7{ "This is a test" };
+
+	cout << s7.find("This") << "\n" << s7.find("is") << "\n" << s7.find("test") << endl;
+	cout << s7.find("is", 4) << endl;
+	cout << s7.substr(0, 4);
+	cout << s7.erase(0, 5) << endl;
+	cout << s2.length() << endl;
+	s2 += " Cena";
+	cout << s2 << endl;
+
+	string testgetline{}, testcin{};
+	cout << "Enter 'John Cena': ";
+	getline(cin, testgetline); //getline gets all input with empty spaces until user press enter
+	cout << testgetline << endl;
+	cout << "Enter 'John Cena': ";
+	cin >> testcin;
+	cout << testcin << "\n================================"<< endl;
+
+	string word{};
+	cin >> word;
+	
+	string findword{ "My name is John Cena!" };
+	size_t position = findword.find(word);
+	if (position != string::npos) {
+		cout << "Found " << word << " at position: " << position << endl;
+	}
+	else {
+		cout << "Sorry, " << word << " not found" << endl;
+	}
+}
