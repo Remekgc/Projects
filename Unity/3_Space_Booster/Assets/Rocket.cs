629ae7ee457ab2b51a3ee91f140167dfaeb76820
Remekgc <RemigiuszGA335@nemi.tskoli.is>
2019-01-09 00:18:12 +0100

    update on movement

--- Unity/3_Space_Booster/Assets/Rocket.cs
+++ Unity/3_Space_Booster/Assets/Rocket.cs
@@ -5,33 +5,61 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float forceThrust = 100f;
+
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput(); 
+        Thrust();
+        Rotate(); 
     }
 
-    private void ProcessInput()
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space)) //Can thurst while rotating
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            float thrustThisFrame = forceThrust * Time.deltaTime; 
+            rigidBody.AddRelativeForce(Vector3.up * thrustThisFrame);
+
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
         }
-        if(Input.GetKey(KeyCode.A))
+        else
         {
-            transform.Rotate(Vector3.forward);
+            audioSource.Stop();
+        }
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; // take manual control of rotation
+
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+        {     
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
+
+        rigidBody.freezeRotation = false; // resume physics control of rotation   
+
     }
+   
 }
