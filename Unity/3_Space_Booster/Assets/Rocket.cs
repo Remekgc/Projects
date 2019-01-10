bf31aa949b4995104a205b98aa30e66ab9168376
Remekgc <remekgc@gmail.com>
2019-01-10 20:15:21 +0100

    Update
    Update on collssion mechamism and added load on hitthing the "Fisnish" tagged platform

--- Unity/3_Space_Booster/Assets/Rocket.cs
+++ Unity/3_Space_Booster/Assets/Rocket.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
@@ -10,6 +8,9 @@ public class Rocket : MonoBehaviour
 
     Rigidbody rigidBody;
     AudioSource audioSource;
+
+    enum State { Alive, Dying, Transcending };
+    State state = State.Alive;
 
     // Start is called before the first frame update
     void Start()
@@ -21,26 +22,44 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Thrust();
-        Rotate(); 
+        if (state == State.Alive)
+        {
+            Thrust();
+            Rotate();
+        }
     }
 
     void OnCollisionEnter(Collision collision)
     {
+        if (state != State.Alive)
+        {
+            return; // ignore collisions when dead
+        }
+
         switch (collision.gameObject.tag)
         {
             case "Friendly":
-                print("ok");
                 //do nothing.
                 break;
-            case "Fuel":
-                print("Fuel");
+            case "Finish":
+                state = State.Transcending;
+                Invoke("LoadNextLevel", 1); // parameterise time
                 break;
             default:
-                print("Dead");
-                //kill player
+                state = State.Dying;
+                Invoke("LoadFirstLevel", 1);
                 break;
         }
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1);
     }
 
     private void Thrust()
