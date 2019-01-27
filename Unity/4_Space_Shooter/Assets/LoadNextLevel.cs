a6795fb6941866437cb95413065bd622bd5b78d4
Remekgc <RemigiuszGA335@nemi.tskoli.is>
2019-01-27 17:57:30 +0100

    Graphics improvements
    Co-Authored-By: bladd35 <bladd35@users.noreply.github.com>

---
+++ Unity/4_Space_Shooter/Assets/LoadNextLevel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNextLevel : MonoBehaviour
+{
+    [SerializeField] float levelLoadDelay = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadLevel", levelLoadDelay);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LoadLevel()
+    {
+        int currentSceenIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceenIndex = currentSceenIndex + 1;
+        if (nextSceenIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceenIndex = 0; //loop back to start
+        }
+        SceneManager.LoadScene(nextSceenIndex);
+    }
+}
