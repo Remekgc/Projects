a27df95a788eb0fdec296f5d23e49e17249fa4ee
Remigiusz Ganiec <remekgc@gmail.com>
2019-11-12 01:13:19 +0100

    OperatorOverloading

---
+++ CPP/OperatorOverloading/OperatorOverloading/MyString.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <iostream>
+
+class MyString
+{
+private:
+	char* str;
+
+public:
+	MyString();
+	MyString(const char* s);
+	MyString(const MyString& source);
+	~MyString();
+	void display() const;
+	int get_length() const;
+	const char* get_str() const;
+};
+
