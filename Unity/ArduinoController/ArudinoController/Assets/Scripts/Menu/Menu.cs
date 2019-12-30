c28d3894961ae77d9db451f1338274aa73276426
Remigiusz Ganiec <remekgc@gmail.com>
2019-12-30 19:46:13 +0100

    Added TextMeshPRO
    Added text mesh pro and Menu scene

---
+++ Unity/ArduinoController/ArudinoController/Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Menu
+{
+    public class Components : MonoBehaviour
+    {
+        public string name;
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
+
