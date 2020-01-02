9659b316532a8e7fde5441bede2f0aa6ec95b555
AdPy97 <58189594+AdPy97@users.noreply.github.com>
2020-01-02 15:41:25 +0100

    Added Android Controls
    First version of controlling sphere by smartphone

---
+++ Unity/ArduinoController/ArudinoController/Assets/Scripts/Player/AndroidControls.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AndroidControls : MonoBehaviour
+{
+    public bool isFlat = true; //device should be on flat
+    private Rigidbody rigid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 tilt = Input.acceleration; //get acceleration of device
+
+        if (isFlat) //if device is on flat turn its acceleration by 90 degrees on X axis
+            tilt = Quaternion.Euler(90, 0, 0) * tilt;
+
+        rigid.AddForce(tilt);
+    }
+}
