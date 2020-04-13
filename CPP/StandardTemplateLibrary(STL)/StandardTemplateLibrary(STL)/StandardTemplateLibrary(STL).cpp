5f67dfdddf85e8fb2d8e718b3e7fdb5611407e4d
Remigiusz Ganiec <remekgc@gmail.com>
2020-04-13 16:00:12 +0200

    Section finished

--- CPP/StandardTemplateLibrary(STL)/StandardTemplateLibrary(STL)/StandardTemplateLibrary(STL).cpp
+++ CPP/StandardTemplateLibrary(STL)/StandardTemplateLibrary(STL)/StandardTemplateLibrary(STL).cpp
@@ -336,7 +336,7 @@ void main()
 {
     //Palindrome();
     PalindromeWithStackAndQueue();
-    //SongListExample();
+    SongListExample();
     NumberOfTimesWordOccuresInFile();
     LineNumbersInWhichWordAppearsInFile();
 }
