16601b0e9322c76de6179ec371f9a2ee72bb6b42
Remigiusz Ganiec <remekgc@gmail.com>
2020-08-17 21:35:13 +0200

    FSM components 0.2

---
+++ Unity/StateMachine(FSM)ForUnityAI/Assets/Scripts/AI_FSM/GameEnvironment.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class GameEnvironment
+{
+    private static GameEnvironment _instance;
+    private List<GameObject> _checkpoints = new List<GameObject>();
+    public List<GameObject> checkpoints { get {return _checkpoints; } }
+
+    public static GameEnvironment Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new GameEnvironment();
+                _instance.checkpoints.AddRange(GameObject.FindGameObjectsWithTag("Checkpoint"));
+            }
+            return _instance;
+        }
+    }
+
+}
