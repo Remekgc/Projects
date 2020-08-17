7724bc40e6f3510d7c7a07757a42d5d7d7c58c13
Remigiusz Ganiec <remekgc@gmail.com>
2020-08-17 21:22:01 +0200

    FSM components 0.1

---
+++ Unity/StateMachine(FSM)ForUnityAI/Assets/Scripts/AI_WaypointsAndGraphs/AgentFollowPath.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+namespace AI_WaypointsAndGraphs
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class AgentFollowPath : MonoBehaviour
+    {
+        public AI_Astar.WaypointManager wpManager;
+        GameObject[] waypoints;
+        [SerializeField] NavMeshAgent agent;
+
+        void Awake()
+        {
+            agent = GetComponent<NavMeshAgent>();
+        }
+
+        void Start()
+        {
+            waypoints = wpManager.waypoints;
+        }
+
+        public void GoToHeli()
+        {
+            agent.SetDestination(waypoints[10].transform.position);
+        }
+
+        public void GoToRuin()
+        {
+            agent.SetDestination(waypoints[3].transform.position);
+        }
+
+        public void GoToFactory()
+        {
+            agent.SetDestination(waypoints[5].transform.position);
+        }
+
+
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                RaycastHit hit;
+
+                if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+                {
+                    agent.SetDestination(hit.point);
+                }
+            }
+        }
+    }
+
+}
---
+++ Unity/StateMachine(FSM)ForUnityAI/Assets/Scripts/AI_WaypointsAndGraphs/AgentFollowPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace AI_WaypointsAndGraphs
+{
+    public class AgentFollowPlayer : MonoBehaviour
+    {
+        [SerializeField] GameObject player;
+        [SerializeField] NavMeshAgent agent;
+
+        void Awake()
+        {
+            if (!agent) agent = GetComponent<NavMeshAgent>();
+        }
+
+        void Start()
+        {
+            if (!player) player = GameObject.FindWithTag("Player");
+        }
+
+        void Update()
+        {
+            agent.SetDestination(player.transform.position);
+        }
+
+    }
+
+}
+
