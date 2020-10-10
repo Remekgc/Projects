644a65a36d66c9e05f6aaa442cd54e7f31e777f6
Remigiusz Ganiec <remekgc@gmail.com>
2020-10-10 15:42:25 +0200

    Game Manager and Timer added

---
+++ Unity/ShooterMechanics/Assets/Main/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.CodeDom;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+    private GameManager() { } // Pervents initialization from outside of this class ;)
+
+    // Results
+    // UIManager
+    [SerializeField] Timer timer;
+
+    private void Awake()
+    {
+        SetupSingleton();
+
+        if (!timer) timer = gameObject.AddComponent<Timer>();
+    }
+
+    private void SetupSingleton()
+    {
+        if (Instance && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        timer.StartTimer();
+    }
+
+}
---
+++ Unity/ShooterMechanics/Assets/Main/Scripts/Trackers/Timer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private bool trakTime = false;
+    [SerializeField] private float time;
+    public float Time => time;
+
+    public void StartTimer()
+    {
+        trakTime = true;
+    }
+
+    public void StopTimer()
+    {
+        trakTime = false;
+    }
+
+    public void ResetTime()
+    {
+        time = 0;
+    }
+
+    private void Update()
+    {
+        if (trakTime)
+        {
+            time += UnityEngine.Time.deltaTime;
+        }
+    }
+}
