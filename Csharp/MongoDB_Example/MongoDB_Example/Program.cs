2d0cf46b5f7416abb8f9eb6cb2538208a3256485
Remigiusz Ganiec <remekgc@gmail.com>
2021-01-31 01:13:08 +0100

    TCP sockets example

---
+++ Csharp/MongoDB_Example/MongoDB_Example/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace MongoDB_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string user, password, databaseName, serverAddress;
+
+            Console.WriteLine("Enter user:");
+            user = Console.ReadLine();
+            Console.WriteLine("Enter pass:");
+            password = Console.ReadLine();
+            Console.WriteLine("Enter server address:");
+            serverAddress = Console.ReadLine();
+            Console.WriteLine("Enter database:");
+            databaseName = Console.ReadLine();
+
+
+            Thread mongoConnection = new Thread(() => TestMongoConnection(user, password, serverAddress, databaseName));
+            mongoConnection.Start();
+
+            Console.WriteLine("press any key to exit");
+            Console.ReadKey();
+
+            mongoConnection.Abort();
+        }
+
+        static void TestMongoConnection(string user, string password, string serverAddress, string databaseName)
+        {
+            var client = new MongoClient($"mongodb+srv://{user}:{password}@{serverAddress}/{databaseName}?retryWrites=true&w=majority");
+            var database = client.GetDatabase("test");
+
+            Console.WriteLine(client.Cluster);
+            Console.WriteLine(database.Client);
+        }
+    }
+}
---
+++ Csharp/TCP_Sockets/TCP_Sockets/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TCP_Sockets
+{
+    class TCP_Client
+    {
+        public void Connect(string server, string message)
+        {
+            try
+            {
+                // Create a TcpClient.
+                // Note, for this client to work you need to have a TcpServer
+                // connected to the same address as specified by the server, port
+                // combination.
+                int port = 13000;
+                TcpClient client = new TcpClient(server, port);
+
+                // Translate the passed message into ASCII and store it as a Byte array.
+                byte[] data = Encoding.ASCII.GetBytes(message);
+
+                // Get a client stream for reading and writing.
+                //  Stream stream = client.GetStream();
+
+                NetworkStream stream = client.GetStream();
+
+                // Send the message to the connected TcpServer.
+                stream.Write(data, 0, data.Length);
+
+                Console.WriteLine("Sent: {0}", message);
+
+                // Receive the TcpServer.response.
+
+                // Buffer to store the response bytes.
+                data = new byte[256];
+
+                // String to store the response ASCII representation.
+                string responseData = string.Empty;
+
+                // Read the first batch of the TcpServer response bytes.
+                int bytes = stream.Read(data, 0, data.Length);
+                responseData = Encoding.ASCII.GetString(data, 0, bytes);
+                Console.WriteLine("Received: {0}", responseData);
+
+                // Close everything.
+                stream.Close();
+                client.Close();
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+        }
+    }
+
+    class TCP_Server
+    {
+        public void StartServer()
+        {
+            TcpListener server = null;
+            try
+            {
+                // Set the TcpListener on port 13000.
+                Int32 port = 13000;
+                IPAddress localAddr = IPAddress.Parse("192.168.1.16");
+
+                // TcpListener server = new TcpListener(port);
+                server = new TcpListener(localAddr, port);
+
+                // Start listening for client requests.
+                server.Start();
+
+                // Buffer for reading data
+                Byte[] bytes = new Byte[256];
+                String data = null;
+
+                // Enter the listening loop.
+                while (true)
+                {
+                    Console.Write("Waiting for a connection... ");
+
+                    // Perform a blocking call to accept requests.
+                    // You could also use server.AcceptSocket() here.
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected!");
+
+                    data = null;
+
+                    // Get a stream object for reading and writing
+                    NetworkStream stream = client.GetStream();
+
+                    int i;
+
+                    // Loop to receive all the data sent by the client.
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        // Translate data bytes to a ASCII string.
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                        Console.WriteLine("Received: {0}", data);
+
+                        // Process the data sent by the client.
+                        data = data.ToUpper();
+
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                        // Send back a response.
+                        stream.Write(msg, 0, msg.Length);
+                        Console.WriteLine("Sent: {0}", data);
+                    }
+
+                    // Shutdown and end connection
+                    client.Close();
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                // Stop listening for new clients.
+                server.Stop();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TCP_Client client = new TCP_Client();
+            TCP_Server server = new TCP_Server();
+
+            Thread serverThread = new Thread(() => server.StartServer());
+            Thread clientThread = new Thread(() => client.Connect("192.168.1.16", "Hola"));
+
+            serverThread.Start();
+            clientThread.Start();
+
+            Thread.Sleep(2000);
+
+            serverThread.Abort();
+            clientThread.Abort();
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
