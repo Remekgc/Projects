0d9efcecaab960dd3822a382d4bf606082311b9b
Remigiusz Ganiec <remekgc@gmail.com>
2023-10-23 18:21:19 +0200

    Updating Space based

---
+++ Unity/ShaderGraph/Assets/Scripts/DebugVertices.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class DebugVertices : MonoBehaviour {
+
+	Mesh mesh;
+	Vector3[] vertices;
+
+	void OnDrawGizmos()
+	{
+		if (vertices == null)
+		{
+			mesh = GetComponent<MeshFilter>().sharedMesh;
+			vertices = mesh.vertices;
+		}
+
+		foreach (Vector3 vertex in vertices)
+		{
+			Vector3 viewVertVector = SceneView.GetAllSceneCameras()[0].WorldToViewportPoint(transform.position + vertex);
+
+			string vert = $"v: {vertex}, ";
+			string worldVert = $"wv: {transform.position + vertex}, ";
+			string viewVert = $"vv: {viewVertVector}";
+
+            Handles.Label(transform.position + vertex, vert + worldVert + viewVert);
+        }
+    }
+}
